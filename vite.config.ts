8328886a53e34516b921546b1d064de38eddebe5
sangin <sangin.kang@firsthabit.com>
2025-08-21 13:30:59 +0900

    fix:vercel deployment file

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/",
   server: {
     host: "::",
     port: 8080,
